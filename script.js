d3849bccc11b36828b0f5e83e472238ba635f28f
Rick <101254647+nauish@users.noreply.github.com>
2023-07-23 17:47:20 +0800

    Complete basic logics

--- script.js
+++ script.js
@@ -5,14 +5,36 @@ const getComputerChoice = () => {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const losingMsg = "You Lose!";
-    const winningMsg = "You Win!";
+    const losingMsg = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    const winningMsg = `You Win! ${playerSelection} beats ${computerSelection}`;
     const drawMsg = "Draw!";
-    if (playerSelection = computerSelection) {
+    const playerSelectionLowercase = `${playerSelection}`.toLowerCase();
+    const errorMsg = "Please enter a valid input"
+    if (choices.indexOf(playerSelectionLowercase) == -1 ) {
+        return errorMsg
+    } else if (playerSelectionLowercase == computerSelection) {
         return drawMsg
+    } else {
+        switch (playerSelectionLowercase) {
+            case 'rock':
+                if (computerSelection == 'scissors') return winningMsg;
+                else return losingMsg;
+            case 'paper':
+                if (computerSelection == 'rock') return winningMsg;
+                else return losingMsg;
+            case 'scissors':
+                if (computerSelection == 'paper') return winningMsg;
+                else return losingMsg;
+        }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Please enter your selection")
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+
+game()
