3f886f47f2190487d45cead703287fe4bd0288ce
Rick <101254647+nauish@users.noreply.github.com>
2023-07-23 16:44:06 +0800

    Add initial logics

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const choices = ['rock', 'paper', 'scissors']
 const getComputerChoice = () => {
-    const choices = ['rock', 'paper', 'scissors']
     const random_choice = Math.floor(Math.random() * choices.length)
     return choices[random_choice]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    const losingMsg = "You Lose!";
+    const winningMsg = "You Win!";
+    const drawMsg = "Draw!";
+    if (playerSelection = computerSelection) {
+        return drawMsg
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
