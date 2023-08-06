76b7eedd03eb77d1aa89ff2acdc29c317a234f48
Rick <101254647+nauish@users.noreply.github.com>
2023-08-06 21:58:01 +0800

    Display scores on the page

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 const choices = ["rock", "paper", "scissors"];
+const winConditions = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
 const getComputerChoice = () => {
   const random_choice = Math.floor(Math.random() * choices.length);
   return choices[random_choice];
 };
 const buttons = document.querySelectorAll("button");
-const playerScore = 0;
-const computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 buttons.forEach((button) =>
   button.addEventListener("click", (e) => {
+    const playerScoreDisplay = document.createElement("p");
+    const computerScoreDisplay = document.createElement("p");
     const div = document.getElementById("result");
-    const result = document.createTextNode(
+    const p = document.createElement("p");
+    const resultText = document.createTextNode(
       playRound(e.target.id, getComputerChoice())
     );
+
+    playerScoreDisplay.textContent = `Player wins ${playerScore} times`;
+    computerScoreDisplay.textContent = `Computer wins ${computerScore} times`;
+
     if (div.hasChildNodes) {
-      div.replaceChildren(result);
+      div.replaceChildren(resultText);
     }
-    div.appendChild(result);
+
+    div.appendChild(resultText);
+    div.appendChild(p);
+    p.appendChild(playerScoreDisplay);
+    p.appendChild(computerScoreDisplay);
   })
 );
 
 function playRound(playerSelection, computerSelection) {
-  const losingMsg = `You Lose! ${computerSelection} beats ${playerSelection}`;
-  const winningMsg = `You Win! ${playerSelection} beats ${computerSelection}`;
-  const drawMsg = `Draw! Both of you use ${computerSelection}`;
   const playerSelectionLowercase = `${playerSelection}`.toLowerCase();
-  const errorMsg = "Please enter a valid input";
 
-  if (choices.indexOf(playerSelectionLowercase) == -1) {
+  const playerWins =
+    winConditions[playerSelectionLowercase] === computerSelection;
+
+  const errorMsg = "Please enter a valid input";
+  if (!choices.includes(playerSelectionLowercase)) {
     return errorMsg;
-  } else if (playerSelectionLowercase == computerSelection) {
+  }
+
+  const drawMsg = `Draw! Both of you use ${computerSelection}`;
+  if (playerSelectionLowercase == computerSelection) {
     return drawMsg;
+  }
+
+  if (playerWins) {
+    playerScore += 1;
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
   } else {
-    switch (playerSelectionLowercase) {
-      case "rock":
-        if (computerSelection == "scissors") {
-          playerScore += 1;
-          return winningMsg;
-        } else {
-          computerScore += 1;
-          return losingMsg;
-        }
-      case "paper":
-        if (computerSelection == "rock") {
-          playerScore += 1;
-          return winningMsg;
-        } else {
-          computerScore += 1;
-          return losingMsg;
-        }
-      case "scissors":
-        if (computerSelection == "paper") {
-          playerScore += 1;
-          return winningMsg;
-        } else {
-          computerScore += 1;
-          return losingMsg;
-        }
-    }
+    computerScore += 1;
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
